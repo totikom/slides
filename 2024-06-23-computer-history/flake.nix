16ed44190f3e48be6fa044cdcb465c432fa3c179
Eugene Lomov <eugene.lomov@protonmail.com>
2026-01-17 12:21:01 +0300

    Pin typst version via flakes

---
+++ 2024-06-23-computer-history/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A flake that loads some packages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            typst
+            typstyle
+          ];
+        };
+      }
+    );
+}
